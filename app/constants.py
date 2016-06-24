759259010fb654e428b92e6d0bcd835f063f6a89
Andreas Küpper <andreas.kupper@yale.edu>
2016-06-24 00:20:51 -0400

    added Gonorrhea and Syphilis

---
+++ app/constants.py
@@ -0,0 +1,209 @@
+
+d_label = np.array(["You", "Your gender", "Your age group", "Your race / ethnicity", "Your location"])
+
+#US statistics
+gender_number = {}
+
+gender_number["Male"] = 155651602
+gender_number["Female"] = 160477237
+
+race_number = {}
+
+race_number["Native"] = 1942876.0
+race_number["Asian"] = 12721721.0
+race_number["Black"] = 29489649.0
+race_number["Hispanic"] = 46407173.0
+race_number["Multiple"] = 5145135.0
+race_number["Pacific"] = 473703.0
+race_number["White"] = 161443167.0
+
+age_number = {}
+
+age_number["0-14"] = 61089123.0
+age_number["15-19"] = 21158964.0
+age_number["20-24"] = 22795438.0
+age_number["25-29"] = 21580198.0
+age_number["30-34"] = 21264389.0
+age_number["35-39"] = 19603770.0
+age_number["40-44"] = 20848920.0
+age_number["45-54"] = 43767532.0
+age_number["55-64"] = 39316431.0
+age_number["65+"] = 44704074.0
+
+
+
+#Chlamydia
+gender_rate = {}
+gender_factor = {}
+
+gender_rate["Male"] = 278.4e-5
+gender_rate["Female"] = 627.2e-5
+
+rate_average = (gender_rate["Male"]*gender_number["Male"]+gender_rate["Female"]*gender_number["Male"])/(gender_number["Male"]+gender_number["Female"])
+
+gender_factor["Male"] = gender_rate["Male"]/rate_average
+gender_factor["Female"] = gender_rate["Female"]/rate_average
+gender_factor["Female"], gender_factor["Male"]
+
+race_rate = {}
+race_factor = {}
+
+race_rate["Native"] = 689.1e-5
+race_rate["Asian"] = 115.8e-5
+race_rate["Black"] = 1152.6e-5
+race_rate["Hispanic"] = 376.2e-5
+race_rate["Multiple"] = 116.1e-5
+race_rate["Pacific"] = 641.5e-5
+race_rate["White"] = 187.0e-5
+
+race_factor["Native"] = race_rate["Native"]/rate_average
+race_factor["Asian"] = race_rate["Asian"]/rate_average
+race_factor["Black"] = race_rate["Black"]/rate_average
+race_factor["Hispanic"] = race_rate["Hispanic"]/rate_average
+race_factor["Multiple"] = race_rate["Multiple"]/rate_average
+race_factor["Pacific"] = race_rate["Pacific"]/rate_average
+race_factor["White"] = race_rate["White"]/rate_average
+
+age_rate = {}
+age_factor = {}
+
+age_rate["0-14"] = 20.0e-5
+age_rate["15-19"] = 1804.0e-5
+age_rate["20-24"] = 2484.6e-5
+age_rate["25-29"] = 1176.2e-5
+age_rate["30-34"] = 532.4e-5
+age_rate["35-39"] = 268.0e-5
+age_rate["40-44"] = 131.5e-5
+age_rate["45-54"] = 56.6e-5
+age_rate["55-64"] = 16.6e-5
+age_rate["65+"] = 3.2e-5
+
+age_factor["0-14"] = age_rate["0-14"]/rate_average
+age_factor["15-19"] = age_rate["15-19"]/rate_average
+age_factor["20-24"] = age_rate["20-24"]/rate_average
+age_factor["25-29"] = age_rate["25-29"]/rate_average
+age_factor["30-34"] = age_rate["30-34"]/rate_average
+age_factor["35-39"] = age_rate["35-39"]/rate_average
+age_factor["40-44"] = age_rate["40-44"]/rate_average
+age_factor["45-54"] = age_rate["45-54"]/rate_average
+age_factor["55-64"] = age_rate["55-64"]/rate_average
+age_factor["65+"] = age_rate["65+"]/rate_average
+
+
+#Gonorrhea
+gender_rate_gonorrhea = {}
+gender_factor_gonorrhea = {}
+
+gender_rate_gonorrhea["Male"] = 120.1e-5
+gender_rate_gonorrhea["Female"] = 101.3e-5
+
+rate_average_gonorrhea = (gender_rate_gonorrhea["Male"]*gender_number["Male"]+gender_rate_gonorrhea["Female"]*gender_number["Male"])/(gender_number["Male"]+gender_number["Female"])
+
+gender_factor_gonorrhea["Male"] = gender_rate_gonorrhea["Male"]/rate_average
+gender_factor_gonorrhea["Female"] = gender_rate_gonorrhea["Female"]/rate_average
+gender_factor_gonorrhea["Female"], gender_factor["Male"]
+
+race_rate_gonorrhea = {}
+race_factor_gonorrhea = {}
+
+race_rate_gonorrhea["Native"] = 103.2e-5
+race_rate_gonorrhea["Asian"] = 19.9e-5
+race_rate_gonorrhea["Black"] = 422.9e-5
+race_rate_gonorrhea["Hispanic"] = 72.7e-5
+race_rate_gonorrhea["Multiple"] = 39.1e-5
+race_rate_gonorrhea["Pacific"] = 103.2e-5
+race_rate_gonorrhea["White"] = 39.8e-5
+
+race_factor_gonorrhea["Native"] = race_rate_gonorrhea["Native"]/rate_average_gonorrhea
+race_factor_gonorrhea["Asian"] = race_rate_gonorrhea["Asian"]/rate_average_gonorrhea
+race_factor_gonorrhea["Black"] = race_rate_gonorrhea["Black"]/rate_average_gonorrhea
+race_factor_gonorrhea["Hispanic"] = race_rate_gonorrhea["Hispanic"]/rate_average_gonorrhea
+race_factor_gonorrhea["Multiple"] = race_rate_gonorrhea["Multiple"]/rate_average_gonorrhea
+race_factor_gonorrhea["Pacific"] = race_rate_gonorrhea["Pacific"]/rate_average_gonorrhea
+race_factor_gonorrhea["White"] = race_rate_gonorrhea["White"]/rate_average_gonorrhea
+
+age_rate_gonorrhea = {}
+age_factor_gonorrhea = {}
+
+age_rate_gonorrhea["0-14"] = 4.3e-5
+age_rate_gonorrhea["15-19"] = 323.6e-5
+age_rate_gonorrhea["20-24"] = 509.8e-5
+age_rate_gonorrhea["25-29"] = 322.5e-5
+age_rate_gonorrhea["30-34"] = 180.6e-5
+age_rate_gonorrhea["35-39"] = 106.1e-5
+age_rate_gonorrhea["40-44"] = 60.9e-5
+age_rate_gonorrhea["45-54"] = 35.0e-5
+age_rate_gonorrhea["55-64"] = 11.6e-5
+age_rate_gonorrhea["65+"] = 2.0e-5
+
+age_factor_gonorrhea["0-14"] = age_rate_gonorrhea["0-14"]/rate_average_gonorrhea
+age_factor_gonorrhea["15-19"] = age_rate_gonorrhea["15-19"]/rate_average_gonorrhea
+age_factor_gonorrhea["20-24"] = age_rate_gonorrhea["20-24"]/rate_average_gonorrhea
+age_factor_gonorrhea["25-29"] = age_rate_gonorrhea["25-29"]/rate_average_gonorrhea
+age_factor_gonorrhea["30-34"] = age_rate_gonorrhea["30-34"]/rate_average_gonorrhea
+age_factor_gonorrhea["35-39"] = age_rate_gonorrhea["35-39"]/rate_average_gonorrhea
+age_factor_gonorrhea["40-44"] = age_rate_gonorrhea["40-44"]/rate_average_gonorrhea
+age_factor_gonorrhea["45-54"] = age_rate_gonorrhea["45-54"]/rate_average_gonorrhea
+age_factor_gonorrhea["55-64"] = age_rate_gonorrhea["55-64"]/rate_average_gonorrhea
+age_factor_gonorrhea["65+"] = age_rate_gonorrhea["65+"]/rate_average_gonorrhea
+
+
+#Syphilis
+gender_rate_syphilis = {}
+gender_factor_syphilis = {}
+
+gender_rate_syphilis["Male"] = 11.7e-5
+gender_rate_syphilis["Female"] = 1.1e-5
+
+rate_average_syphilis = (gender_rate_syphilis["Male"]*gender_number["Male"]+gender_rate_syphilis["Female"]*gender_number["Male"])/(gender_number["Male"]+gender_number["Female"])
+
+gender_factor_syphilis["Male"] = gender_rate_syphilis["Male"]/rate_average
+gender_factor_syphilis["Female"] = gender_rate_syphilis["Female"]/rate_average
+gender_factor_syphilis["Female"], gender_factor["Male"]
+
+race_rate_syphilis = {}
+race_factor_syphilis = {}
+
+race_rate_syphilis["Native"] = 7.9e-5
+race_rate_syphilis["Asian"] = 2.8e-5
+race_rate_syphilis["Black"] = 18.9e-5
+race_rate_syphilis["Hispanic"] = 7.4e-5
+race_rate_syphilis["Multiple"] = 2.3e-5
+race_rate_syphilis["Pacific"] = 6.7e-5
+race_rate_syphilis["White"] = 3.4e-5
+
+race_factor_syphilis["Native"] = race_rate_syphilis["Native"]/rate_average_syphilis
+race_factor_syphilis["Asian"] = race_rate_syphilis["Asian"]/rate_average_syphilis
+race_factor_syphilis["Black"] = race_rate_syphilis["Black"]/rate_average_syphilis
+race_factor_syphilis["Hispanic"] = race_rate_syphilis["Hispanic"]/rate_average_syphilis
+race_factor_syphilis["Multiple"] = race_rate_syphilis["Multiple"]/rate_average_syphilis
+race_factor_syphilis["Pacific"] = race_rate_syphilis["Pacific"]/rate_average_syphilis
+race_factor_syphilis["White"] = race_rate_syphilis["White"]/rate_average_syphilis
+
+age_rate_syphilis = {}
+age_factor_syphilis = {}
+
+age_rate_syphilis["0-14"] = 0.0e-5
+age_rate_syphilis["15-19"] = 4.8e-5
+age_rate_syphilis["20-24"] = 18.1e-5
+age_rate_syphilis["25-29"] = 19.0e-5
+age_rate_syphilis["30-34"] = 13.6e-5
+age_rate_syphilis["35-39"] = 10.4e-5
+age_rate_syphilis["40-44"] = 8.4e-5
+age_rate_syphilis["45-54"] = 6.8e-5
+age_rate_syphilis["55-64"] = 2.3e-5
+age_rate_syphilis["65+"] = 0.4e-5
+
+age_factor_syphilis["0-14"] = age_rate_syphilis["0-14"]/rate_average_syphilis
+age_factor_syphilis["15-19"] = age_rate_syphilis["15-19"]/rate_average_syphilis
+age_factor_syphilis["20-24"] = age_rate_syphilis["20-24"]/rate_average_syphilis
+age_factor_syphilis["25-29"] = age_rate_syphilis["25-29"]/rate_average_syphilis
+age_factor_syphilis["30-34"] = age_rate_syphilis["30-34"]/rate_average_syphilis
+age_factor_syphilis["35-39"] = age_rate_syphilis["35-39"]/rate_average_syphilis
+age_factor_syphilis["40-44"] = age_rate_syphilis["40-44"]/rate_average_syphilis
+age_factor_syphilis["45-54"] = age_rate_syphilis["45-54"]/rate_average_syphilis
+age_factor_syphilis["55-64"] = age_rate_syphilis["55-64"]/rate_average_syphilis
+age_factor_syphilis["65+"] = age_rate_syphilis["65+"]/rate_average_syphilis
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,104 +1,37 @@
 from flask import render_template, request, send_file
 from app import app
-#from sqlalchemy import create_engine
-#from sqlalchemy_utils import database_exists, create_database
 from io import BytesIO
 import pandas as pd
-#import psycopg2
 import numpy as np
 import pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
-#import base64
+#from sqlalchemy import create_engine
+#from sqlalchemy_utils import database_exists, create_database
+#import psycopg2
+
+exec(open("app/constants.py").read())
+
+#Seaborn options
 sns.set(style="white", context="talk")
-
-
-#d = np.array([0.1, 0.2, 0.1, 0.4, 0.3])
 d_label = np.array(["You", "Your gender", "Your age group", "Your race / ethnicity", "Your location"])
 
+#load csv files
 df_zipcode = pd.read_csv("/Users/akuepper/Desktop/Insight/project/data/census_zipcode.csv")
 df_zipcode_unnormalized = pd.read_csv("/Users/akuepper/Desktop/Insight/project/data/census_zipcode_unnormalized.csv")
 model = pickle.load(open("/Users/akuepper/Desktop/Insight/project/data/randomforest_params.pickle", "rb"))
 Ymean = pickle.load(open('/Users/akuepper/Desktop/Insight/project/data/Ymean.pickle', "rb"))
 Ystd = pickle.load(open('/Users/akuepper/Desktop/Insight/project/data/Ystd.pickle', "rb"))
+model_gonorrhea = pickle.load(open("/Users/akuepper/Desktop/Insight/project/data/randomforest_params_gonorrhea.pickle", "rb"))
+Ymean_gonorrhea = pickle.load(open('/Users/akuepper/Desktop/Insight/project/data/Ymean_gonorrhea.pickle', "rb"))
+Ystd_gonorrhea = pickle.load(open('/Users/akuepper/Desktop/Insight/project/data/Ystd_gonorrhea.pickle', "rb"))
+model_syphilis = pickle.load(open("/Users/akuepper/Desktop/Insight/project/data/randomforest_params_syphilis.pickle", "rb"))
+Ymean_syphilis = pickle.load(open('/Users/akuepper/Desktop/Insight/project/data/Ymean_syphilis.pickle', "rb"))
+Ystd_syphilis = pickle.load(open('/Users/akuepper/Desktop/Insight/project/data/Ystd_syphilis.pickle', "rb"))
 
 df_sites = pd.read_csv('/Users/akuepper/Desktop/Insight/project/data/testingsites.csv')
 
-gender_rate = {}
-gender_factor = {}
-gender_number = {}
-gender_rate["Male"] = 278.4e-5
-gender_rate["Female"] = 627.2e-5
-gender_number["Male"] = 155651602
-gender_number["Female"] = 160477237
-rate_average = (gender_rate["Male"]*gender_number["Male"]+gender_rate["Female"]*gender_number["Male"])/(gender_number["Male"]+gender_number["Female"])
-gender_factor["Male"] = gender_rate["Male"]/rate_average
-gender_factor["Female"] = gender_rate["Female"]/rate_average
-gender_factor["Female"], gender_factor["Male"]
-race_rate = {}
-race_factor = {}
-race_number = {}
-race_number["Native"] = 1942876.0
-race_number["Asian"] = 12721721.0
-race_number["Black"] = 29489649.0
-race_number["Hispanic"] = 46407173.0
-race_number["Multiple"] = 5145135.0
-race_number["Pacific"] = 473703.0
-race_number["White"] = 161443167.0
-race_rate["Native"] = 689.1e-5
-race_rate["Asian"] = 115.8e-5
-race_rate["Black"] = 1152.6e-5
-race_rate["Hispanic"] = 376.2e-5
-race_rate["Multiple"] = 116.1e-5
-race_rate["Pacific"] = 641.5e-5
-race_rate["White"] = 187.0e-5
-US_number = race_number["Native"] + race_number["Asian"] + race_number["Black"] + race_number["Hispanic"] + race_number["Multiple"] + race_number["Pacific"] + race_number["White"]
-rate_average = (race_rate["Native"]*race_number["Native"]+race_rate["Asian"]*race_number["Asian"]+race_rate["Black"]*race_number["Black"]+race_rate["Hispanic"]*race_number["Hispanic"]+race_rate["Multiple"]*race_number["Multiple"]+race_rate["Pacific"]*race_number["Multiple"]+race_rate["White"]*race_number["White"])/US_number  
-race_factor["Native"] = race_rate["Native"]/rate_average
-race_factor["Asian"] = race_rate["Asian"]/rate_average
-race_factor["Black"] = race_rate["Black"]/rate_average
-race_factor["Hispanic"] = race_rate["Hispanic"]/rate_average
-race_factor["Multiple"] = race_rate["Multiple"]/rate_average
-race_factor["Pacific"] = race_rate["Pacific"]/rate_average
-race_factor["White"] = race_rate["White"]/rate_average
 
-age_rate = {}
-age_factor = {}
-age_number = {}
-age_number["0-14"] = 61089123.0
-age_number["15-19"] = 21158964.0
-age_number["20-24"] = 22795438.0
-age_number["25-29"] = 21580198.0
-age_number["30-34"] = 21264389.0
-age_number["35-39"] = 19603770.0
-age_number["40-44"] = 20848920.0
-age_number["45-54"] = 43767532.0
-age_number["55-64"] = 39316431.0
-age_number["65+"] = 44704074.0
-
-age_rate["0-14"] = 20.0e-5
-age_rate["15-19"] = 1804.0e-5
-age_rate["20-24"] = 2484.6e-5
-age_rate["25-29"] = 1176.2e-5
-age_rate["30-34"] = 532.4e-5
-age_rate["35-39"] = 268.0e-5
-age_rate["40-44"] = 131.5e-5
-age_rate["45-54"] = 56.6e-5
-age_rate["55-64"] = 16.6e-5
-age_rate["65+"] = 3.2e-5
-
-US_age_number = age_number["0-14"] + age_number["15-19"] + age_number["20-24"] + age_number["25-29"] + age_number["30-34"] + age_number["35-39"] + age_number["40-44"] + age_number["45-54"] + age_number["55-64"] + age_number["65+"]
-rate_average = (age_rate["0-14"]*age_number["0-14"]+age_rate["15-19"]*age_number["15-19"]+age_rate["20-24"]*age_number["20-24"]+age_rate["25-29"]*age_number["25-29"]+age_rate["30-34"]*age_number["30-34"]+age_rate["35-39"]*age_number["35-39"]+age_rate["40-44"]*age_number["40-44"]+age_rate["45-54"]*age_number["45-54"]+age_rate["55-64"]*age_number["55-64"]+age_rate["65+"]*age_number["65+"])/US_age_number  
-age_factor["0-14"] = age_rate["0-14"]/rate_average
-age_factor["15-19"] = age_rate["15-19"]/rate_average
-age_factor["20-24"] = age_rate["20-24"]/rate_average
-age_factor["25-29"] = age_rate["25-29"]/rate_average
-age_factor["30-34"] = age_rate["30-34"]/rate_average
-age_factor["35-39"] = age_rate["35-39"]/rate_average
-age_factor["40-44"] = age_rate["40-44"]/rate_average
-age_factor["45-54"] = age_rate["45-54"]/rate_average
-age_factor["55-64"] = age_rate["55-64"]/rate_average
-age_factor["65+"] = age_rate["65+"]/rate_average
 
 
 def lookup_std_testing_site(Zipcode):
@@ -116,18 +49,29 @@ def lookup_std_testing_site(Zipcode):
     return sentence
 
 
+
 def calculate_rate(Zipcode):
     target = df_zipcode[df_zipcode["geoid2"]==Zipcode]
     target_params = target.values[0]
     chlamydia_rate = model.predict(target_params[1:])*Ystd+Ymean
     return chlamydia_rate
 
+def calculate_rate_gonorrhea(Zipcode):
+    target = df_zipcode[df_zipcode["geoid2"]==Zipcode]
+    target_params = target.values[0]
+    gonorrhea_rate = model_gonorrhea.predict(target_params[1:])*Ystd_gonorrhea+Ymean_gonorrhea
+    return gonorrhea_rate
+
+def calculate_rate_syphilis(Zipcode):
+    target = df_zipcode[df_zipcode["geoid2"]==Zipcode]
+    target_params = target.values[0]
+    syphilis_rate = model_syphilis.predict(target_params[1:])*Ystd_syphilis+Ymean_syphilis
+    return syphilis_rate
 
 
 @app.route('/chlamydiaplot/you/<float:yourrate>/gender/<float:genderrate>/age/<float:agerate>/race/<float:racerate>/location/<float:locationrate>')
 def make_chlamydia_plot(yourrate, genderrate, agerate, racerate, locationrate):
   
- # d = pickle.load(open('/Users/akuepper/Desktop/Insight/project/tmp/userdata.pickle', "rb"))
   d = np.array([yourrate, genderrate, agerate, racerate, locationrate])
   print(d)
 
@@ -157,51 +101,20 @@ def make_chlamydia_plot(yourrate, genderrate, agerate, racerate, locationrate):
 #  return base64.b64encode(img.getvalue())
 
 
-@app.route('/syphilisplot')
-def make_syphilis_plot():
+@app.route('/gonorrheaplot/you/<float:yourrate_gonorrhea>/gender/<float:genderrate_gonorrhea>/age/<float:agerate_gonorrhea>/race/<float:racerate_gonorrhea>/location/<float:locationrate_gonorrhea>')
+def make_gonorrhea_plot(yourrate_gonorrhea, genderrate_gonorrhea, agerate_gonorrhea, racerate_gonorrhea, locationrate_gonorrhea):
   
-  d = np.random.rand(5)#pickle.load(open('/Users/akuepper/Desktop/Insight/project/tmp/userdata.pickle', "rb"))
+  d_gonorrhea = np.array([yourrate_gonorrhea, genderrate_gonorrhea, agerate_gonorrhea, racerate_gonorrhea, locationrate_gonorrhea])
+ 
+  print(d_gonorrhea)
 
-  print(d)
-  #make Chlamydia plot
+  #make Gonorrhea plot
   fig, ax = plt.subplots(1, 1, figsize=(10, 6), sharex=True)
 
   # Center the data to make it diverging
-  y2 = d   #- d.mean()
+  y2_gonorrhea = d_gonorrhea  # - d.mean()
 
-  sns.barplot(d_label, y2, palette="RdBu_r", ax=ax)
-
-  ax.set_ylabel('Risk', fontsize=25)
-#  ax.plot([-1, len(d)], [0,0], "k-", linewidth=1.0)
-
- # plt.title(r'Syphilis', fontsize=25)
-
-  # Finalize the plot
-  sns.despine(bottom=True)
-  plt.setp(fig.axes, yticks=[])
-  plt.tight_layout(h_pad=3)
-
-  img = BytesIO()
-  fig.savefig(img)
-  img.seek(0)
-#  plt.savefig('/Users/akuepper/Desktop/Insight/project/app/static/img/chlamydia.png', bbox_inches='tight', dpi=150)
-  return send_file(img, mimetype='image/png')
-#  return base64.b64encode(img.getvalue())
-
-
-@app.route('/gonorrheaplot')
-def make_gonorrhea_plot():
-  
-  d = np.random.rand(5)#pickle.load(open('/Users/akuepper/Desktop/Insight/project/tmp/userdata.pickle', "rb"))
-
-  print(d)
-  #make Chlamydia plot
-  fig, ax = plt.subplots(1, 1, figsize=(10, 6), sharex=True)
-
-  # Center the data to make it diverging
-  y2 = d  # - d.mean()
-
-  sns.barplot(d_label, y2, palette="RdBu_r", ax=ax)
+  sns.barplot(d_label, y2_gonorrhea, palette="RdBu_r", ax=ax)
 
   ax.set_ylabel('Risk', fontsize=25)
 #  ax.plot([-1, len(d)], [0,0], "k-", linewidth=1.0)
@@ -221,24 +134,25 @@ def make_gonorrhea_plot():
 #  return base64.b64encode(img.getvalue())
 
 
-@app.route('/hivplot')
-def make_hiv_plot():
+@app.route('/syphilisplot/you/<float:yourrate_syphilis>/gender/<float:genderrate_syphilis>/age/<float:agerate_syphilis>/race/<float:racerate_syphilis>/location/<float:locationrate_syphilis>')
+def make_syphilis_plot(yourrate_syphilis, genderrate_syphilis, agerate_syphilis, racerate_syphilis, locationrate_syphilis):
   
-  d = np.random.rand(5)#pickle.load(open('/Users/akuepper/Desktop/Insight/project/tmp/userdata.pickle', "rb"))
+  d_syphilis = np.array([yourrate_syphilis, genderrate_syphilis, agerate_syphilis, racerate_syphilis, locationrate_syphilis])
 
-  print(d)
-  #make Chlamydia plot
+  print(d_syphilis)
+
+  #make Syphilis plot
   fig, ax = plt.subplots(1, 1, figsize=(10, 6), sharex=True)
 
   # Center the data to make it diverging
-  y2 = d - d.mean()
+  y2_syphilis = d_syphilis   #- d.mean()
 
-  sns.barplot(d_label, y2, palette="RdBu_r", ax=ax)
+  sns.barplot(d_label, y2_syphilis, palette="RdBu_r", ax=ax)
 
-  ax.set_ylabel('-        Risk        +', fontsize=25)
-  ax.plot([-1, len(d)], [0,0], "k-", linewidth=1.0)
+  ax.set_ylabel('Risk', fontsize=25)
+#  ax.plot([-1, len(d)], [0,0], "k-", linewidth=1.0)
 
-#  plt.title(r'HIV', fontsize=25)
+ # plt.title(r'Syphilis', fontsize=25)
 
   # Finalize the plot
   sns.despine(bottom=True)
@@ -251,6 +165,8 @@ def make_hiv_plot():
 #  plt.savefig('/Users/akuepper/Desktop/Insight/project/app/static/img/chlamydia.png', bbox_inches='tight', dpi=150)
   return send_file(img, mimetype='image/png')
 #  return base64.b64encode(img.getvalue())
+
+
 
 
 
@@ -375,21 +291,36 @@ def stdme_output():
   agerate = age_rate[age]*100
   racerate = race_rate[race]*100
 
-  print(zipcoderate, TOTALNR.values, genderrate, GENDERNR.values, racerate, RACENR.values, agerate, AGENR.values)
+  zipcoderate_gonorrhea = calculate_rate_gonorrhea(zipcode)*100
+  genderrate_gonorrhea = gender_rate_gonorrhea[gender]*100
+  agerate_gonorrhea = age_rate_gonorrhea[age]*100
+  racerate_gonorrhea = race_rate_gonorrhea[race]*100
+
+  zipcoderate_syphilis = calculate_rate_syphilis(zipcode)*100
+  genderrate_syphilis = gender_rate_syphilis[gender]*100
+  agerate_syphilis = age_rate_syphilis[age]*100
+  racerate_syphilis = race_rate_syphilis[race]*100
+
+
+  print("Chlamydia: ", zipcoderate, TOTALNR.values, genderrate, GENDERNR.values, racerate, RACENR.values, agerate, AGENR.values)
+  print("Gonorrhea: ", zipcoderate_gonorrhea, TOTALNR.values, genderrate_gonorrhea, GENDERNR.values, racerate_gonorrhea, RACENR.values, agerate_gonorrhea, AGENR.values)
+  print("Syphilis: ", zipcoderate_syphilis, TOTALNR.values, genderrate_syphilis, GENDERNR.values, racerate_syphilis, RACENR.values, agerate_syphilis, AGENR.values)
+
   the_result = (zipcoderate/TOTALNR.values + genderrate/GENDERNR.values + racerate/RACENR.values + agerate/AGENR.values)/(1.0/TOTALNR.values+1.0/GENDERNR.values+1.0/RACENR.values+1.0/AGENR.values)
+  the_result_gonorrhea = (zipcoderate_gonorrhea/TOTALNR.values + genderrate_gonorrhea/GENDERNR.values + racerate_gonorrhea/RACENR.values + agerate_gonorrhea/AGENR.values)/(1.0/TOTALNR.values+1.0/GENDERNR.values+1.0/RACENR.values+1.0/AGENR.values)
+  the_result_syphilis = (zipcoderate_syphilis/TOTALNR.values + genderrate_syphilis/GENDERNR.values + racerate_syphilis/RACENR.values + agerate_syphilis/AGENR.values)/(1.0/TOTALNR.values+1.0/GENDERNR.values+1.0/RACENR.values+1.0/AGENR.values)
 
   d = np.array([the_result[0], genderrate, agerate, racerate, zipcoderate[0]])
-
-  with open("/Users/akuepper/Desktop/Insight/project/tmp/userdata.pickle", "wb") as userfile:
-    pickle.dump(d, userfile)
+  d_gonorrhea = np.array([the_result_gonorrhea[0], genderrate_gonorrhea, agerate_gonorrhea, racerate_gonorrhea, zipcoderate_gonorrhea[0]])
+  d_syphilis = np.array([the_result_syphilis[0], genderrate_syphilis, agerate_syphilis, racerate_syphilis, zipcoderate_syphilis[0]])
   
   sentence = lookup_std_testing_site(zipcode)
 
-  if d[0]/d[4] > 5.0:
+  if d[0]/(100*rate_average) > 3.0:
     warning = "VERY HIGH"
-  elif d[0]/d[4] > 2.0:
+  elif d[0]/(100*rate_average) > 2.0:
     warning = "Very high"
-  elif d[0]/d[4] > 1.0:
+  elif d[0]/(100*rate_average) > 1.0:
     warning = "High"
   else:
     warning = "Lower than average"
@@ -397,5 +328,29 @@ def stdme_output():
   nthperson = int(100.0/d[4])
 
 
-  return render_template("output.html", the_result = np.round(the_result[0],decimals=2), average = np.round(zipcoderate[0],decimals=2), zipcode = zipcode_string, sentence = sentence, yourrate = d[0], genderrate = d[1], agerate = d[2], racerate = d[3], locationrate = d[4], warning = warning, nthperson = nthperson)
+  if d_gonorrhea[0]/(100*rate_average_gonorrhea) > 3.0:
+    warning_gonorrhea = "VERY HIGH"
+  elif d_gonorrhea[0]/(100*rate_average_gonorrhea) > 2.0:
+    warning_gonorrhea = "Very high"
+  elif d_gonorrhea[0]/(100*rate_average_gonorrhea) > 1.0:
+    warning_gonorrhea = "High"
+  else:
+    warning_gonorrhea = "Lower than average"
+
+  nthperson_gonorrhea = int(100.0/d_gonorrhea[4])
+
+
+  if d_syphilis[0]/(100*rate_average_syphilis) > 3.0:
+    warning_syphilis = "VERY HIGH"
+  elif d_syphilis[0]/(100*rate_average_syphilis) > 2.0:
+    warning_syphilis = "Very high"
+  elif d_syphilis[0]/(100*rate_average_syphilis) > 1.0:
+    warning_syphilis = "High"
+  else:
+    warning_syphilis = "Lower than average"
+
+  nthperson_syphilis = int(100.0/d_syphilis[4])
+
+
+  return render_template("output.html", the_result = np.round(the_result[0],decimals=2), average = np.round(zipcoderate[0],decimals=2), zipcode = zipcode_string, sentence = sentence, yourrate = d[0], genderrate = d[1], agerate = d[2], racerate = d[3], locationrate = d[4], warning = warning, nthperson = nthperson, national_average = np.round(rate_average*100, decimals=2), the_result_gonorrhea = np.round(the_result_gonorrhea[0],decimals=2), average_gonorrhea = np.round(zipcoderate_gonorrhea[0],decimals=2), yourrate_gonorrhea = d_gonorrhea[0], genderrate_gonorrhea = d_gonorrhea[1], agerate_gonorrhea = d_gonorrhea[2], racerate_gonorrhea = d_gonorrhea[3], locationrate_gonorrhea = d_gonorrhea[4], warning_gonorrhea = warning_gonorrhea, nthperson_gonorrhea = nthperson_gonorrhea, national_average_gonorrhea = np.round(rate_average_gonorrhea*100, decimals=2), the_result_syphilis = np.round(the_result_syphilis[0],decimals=2), average_syphilis = np.round(zipcoderate_syphilis[0],decimals=2), yourrate_syphilis = d_syphilis[0], genderrate_syphilis = d_syphilis[1], agerate_syphilis = d_syphilis[2], racerate_syphilis = d_syphilis[3], locationrate_syphilis = d_syphilis[4], warning_syphilis = warning_syphilis, nthperson_syphilis = nthperson_syphilis, national_average_syphilis = np.round(rate_average_syphilis*100, decimals=2))
 
