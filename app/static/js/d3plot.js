73a0d68fd1fb7f39ce10ede92551dbfa34f33e25
Andreas Küpper <andreas.kupper@yale.edu>
2016-06-29 23:18:01 -0400

    updated D3 plot scaling

--- app/static/js/d3plot.js
+++ app/static/js/d3plot.js
@@ -1,5 +1,5 @@
-var width = 960,
-    height = 600;
+var width = window.innerWidth*0.9,
+    height = width/960*450;
 
 var rateById = d3.map();
 
@@ -8,7 +8,7 @@ var quantize = d3.scale.quantize()
     .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
 
 var projection = d3.geo.albersUsa()
-    .scale(1280)
+    .scale(width*1.0)
     .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
@@ -25,6 +25,7 @@ queue()
 
 function ready(error, us) {
   if (error) throw error;
+  d3.select(window).on("resize", resize);
 
   svg.append("g")
       .attr("class", "counties")
@@ -38,6 +39,15 @@ function ready(error, us) {
       .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
       .attr("class", "states")
       .attr("d", path);
+
+  window.addEventListener('resize', resize); 
+
+  function resize() {
+    width = window.innerWidth*0.9, height = width/960*450;
+    svg.attr("width", width).attr("height", height);
+    force.size([width, height]).resume();
+  }
+
 }
 
 d3.select(self.frameElement).style("height", height + "px");
